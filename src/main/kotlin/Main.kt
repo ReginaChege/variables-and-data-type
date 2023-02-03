4c1d2568cfd2182ca7cf5d369f8856d05af06521
ReginaChege <reginachegewairimu@gmail.com>
2023-02-03 06:04:53 +0300

    fight message

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+        var studentDetails = arrayOf("Ann Owoko","Eunice Ngendo","Robert Chege")
+        println(studentDetails)
+        var ages= arrayOf(19,20,23,22)
+        println(ages)
+        var phoneNumber= arrayOf("794363077","723456890","798769342")
+        println(phoneNumber)
+        var weightkg: Byte = 40
+        println(weightkg)
+        var citizenshipKenyan: Boolean = true
+        println(citizenshipKenyan)
+        var citizenshipNotKenyan: Boolean =false
+        println(citizenshipNotKenyan)
+        var student1 = arrayOf("studentDetails(0)","age","phoneNumber","weightkg","citizenshipKenyan")
+        var student2 = arrayOf("studentDetails(1)","age","phoneNumber","weightkg","citizenshipKenyan")
+        var student3 = arrayOf("studentDetails(2)","ages","phoneNumber","weightkg","citizenshipNotKenyan")
+
+clear
+
+    }
